2ba17c43adcaac3608d601254c2c3da1acadc8b8
laineantti <laineantti@users.noreply.github.com>
2020-10-22 12:23:53 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+const [ruutu1, setRuutu1] = useState("")
+const [ruutu2, setRuutu2] = useState("")
+const [ruutu3, setRuutu3] = useState("")
+const [ruutu4, setRuutu4] = useState("")
+const [ruutu5, setRuutu5] = useState("")
+const [ruutu6, setRuutu6] = useState("")
+const [ruutu7, setRuutu7] = useState("")
+const [ruutu8, setRuutu8] = useState("")
+const [ruutu9, setRuutu9] = useState("")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div class="ruutu" id="ruutu1" onClick={()=>setRuutu1()} value={ruutu1}></div>
+      <div class="ruutu" id="ruutu2" value={ruutu2}></div>
+      <div class="ruutu" id="ruutu3" value={ruutu3}></div>
+      <div class="ruutu" id="ruutu4" value={ruutu4}></div>
+      <div class="ruutu" id="ruutu5" value={ruutu5}></div>
+      <div class="ruutu" id="ruutu6" value={ruutu6}></div>
+      <div class="ruutu" id="ruutu7" value={ruutu7}></div>
+      <div class="ruutu" id="ruutu8" value={ruutu8}></div>
+      <div class="ruutu" id="ruutu9" value={ruutu9}></div>
     </div>
   );
 }
