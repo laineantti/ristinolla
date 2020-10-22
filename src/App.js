f2c3207edd662aafa8d06685bfe469d24c926e62
laineantti <laineantti@users.noreply.github.com>
2020-10-22 19:33:50 +0300

    Melkein toimiva

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import React, { useState } from "react";
 
 function App() {
-  const [teksti, setTeksti] = useState("")
-
   const [vuoro, setVuoro] = useState("");
   const [voittaja, setVoittaja] = useState("");
-  const [peliloppui, setPeliloppui] = useState(false);
-
-  // alussa arvotaan X tai O aloittajaksi
-  if (vuoro === "") {
-    const aloittaja = Math.random() >= 0.5;
-    if (aloittaja === true) setVuoro("X");
-    else setVuoro("O");
-  }
 
   // lainausmerkkien välissä oleva " " on tyhjä merkki (ei välilyönti), että nappi näyttää järkevämmältä tyhjänä
   const [ruutu1, setRuutu1] = useState(" ");
@@ -25,60 +15,67 @@ function App() {
   const [ruutu8, setRuutu8] = useState(" ");
   const [ruutu9, setRuutu9] = useState(" ");
 
-  const ruutuaKlikattu = (event) => {
+  // alussa arvotaan X tai O aloittajaksi
+  if (vuoro === "") {
+    const aloittaja = Math.random() >= 0.5;
+    if (aloittaja === true) setVuoro("X");
+    else setVuoro("O");
+  }
+
+  const pelaajanVuoro = (event) => {
     switch (event.target.id) {
       case "1":
         if (ruutu1 === " ") {
           setRuutu1(vuoro);
-          voittikoJoku();
+          vaihdaVuoroa();
         }
         break;
       case "2":
         if (ruutu2 === " ") {
           setRuutu2(vuoro);
-          muutaVuoro();
+          vaihdaVuoroa();
         }
         break;
       case "3":
         if (ruutu3 === " ") {
           setRuutu3(vuoro);
-          muutaVuoro();
+          vaihdaVuoroa();
         }
         break;
       case "4":
         if (ruutu4 === " ") {
           setRuutu4(vuoro);
-          muutaVuoro();
+          vaihdaVuoroa();
         }
         break;
       case "5":
         if (ruutu5 === " ") {
           setRuutu5(vuoro);
-          muutaVuoro();
+          vaihdaVuoroa();
         }
         break;
       case "6":
         if (ruutu6 === " ") {
           setRuutu6(vuoro);
-          muutaVuoro();
+          vaihdaVuoroa();
         }
         break;
       case "7":
         if (ruutu7 === " ") {
           setRuutu7(vuoro);
-          muutaVuoro();
+          vaihdaVuoroa();
         }
         break;
       case "8":
         if (ruutu8 === " ") {
           setRuutu8(vuoro);
-          muutaVuoro();
+          vaihdaVuoroa();
         }
         break;
       case "9":
         if (ruutu9 === " ") {
           setRuutu9(vuoro);
-          muutaVuoro();
+          vaihdaVuoroa();
         }
         break;
       default:
@@ -87,63 +84,112 @@ function App() {
     }
   };
 
-  const muutaVuoro = () => {
+  const vaihdaVuoroa = () => {
+    voitonTarkistaminen();
     if (vuoro === "X") setVuoro("O");
     else setVuoro("X");
   };
 
-  const voittikoJoku = () => {
-    if (ruutu1 !== " ")
+  const voitonTarkistaminen = () => {
+    // VAAKATASOSSA
+    //==================================================
+    // ylä
+    if (ruutu1 !== " ") {
       if (ruutu1 === ruutu2 && ruutu2 === ruutu3) {
-        setPeliloppui(true);
-        setVoittaja(ruutu1);
-        setTeksti("Nyt on pelaajan <b>{vuoro}</b> vuoro!")
+        setVoittaja(vuoro);
       }
-      else muutaVuoro();
+    }
+    // keski
+    if (ruutu3 !== " ") {
+      if (ruutu3 === ruutu4 && ruutu4 === ruutu5) {
+        setVoittaja(vuoro);
+      }
+    }
+    // ala
+    if (ruutu7 !== " ") {
+      if (ruutu7 === ruutu8 && ruutu8 === ruutu9) {
+        setVoittaja(vuoro);
+      }
+    }
+    // SARAKKEET PYSTYSSÄ
+    //==================================================
+    // vasen
+    if (ruutu1 !== " ") {
+      if (ruutu1 === ruutu4 && ruutu4 === ruutu7) {
+        setVoittaja(vuoro);
+      }
+    }
+    // keski
+    if (ruutu2 !== " ") {
+      if (ruutu2 === ruutu5 && ruutu5 === ruutu8) {
+        setVoittaja(vuoro);
+      }
+    }
+    // oikea
+    if (ruutu3 !== " ") {
+      if (ruutu3 === ruutu6 && ruutu6 === ruutu9) {
+        setVoittaja(vuoro);
+      }
+    }
+    // VINOTTAIN
+    //==================================================
+    // vasemmasta ylänurkasta oikeaan alanurkkaan
+    if (ruutu1 !== " ") {
+      if (ruutu1 === ruutu5 && ruutu5 === ruutu9) {
+        setVoittaja(vuoro);
+      }
+    }
+    // oikeasta ylänurkasta vasempaan alanurkkaan
+    if (ruutu3 !== " ") {
+      if (ruutu3 === ruutu5 && ruutu5 === ruutu7) {
+        setVoittaja(vuoro);
+      }
+    }
   };
 
-
-  if (peliloppui) {
+  if (voittaja !== "") {
     return (
-      <p id="tilanne">
-        Pelaaja <b>{voittaja}</b> voitti! Peli on päättynyt!
+      <p>
+        Pelaaja <b>{vuoro}</b> voitti! Peli on päättynyt!
       </p>
     );
   } else {
     return (
       <div>
-        <p id="tilanne">{teksti}</p>
+        <p>
+          Pelaajan <b>{vuoro}</b> vuoro.
+        </p>
         <div>
           <div>
-            <button onClick={(event) => ruutuaKlikattu(event)} id="1">
+            <button onClick={(event) => pelaajanVuoro(event)} id="1">
               {ruutu1}
             </button>
-            <button onClick={(event) => ruutuaKlikattu(event)} id="2">
+            <button onClick={(event) => pelaajanVuoro(event)} id="2">
               {ruutu2}
             </button>
-            <button onClick={(event) => ruutuaKlikattu(event)} id="3">
+            <button onClick={(event) => pelaajanVuoro(event)} id="3">
               {ruutu3}
             </button>
           </div>
           <div>
-            <button onClick={(event) => ruutuaKlikattu(event)} id="4">
+            <button onClick={(event) => pelaajanVuoro(event)} id="4">
               {ruutu4}
             </button>
-            <button onClick={(event) => ruutuaKlikattu(event)} id="5">
+            <button onClick={(event) => pelaajanVuoro(event)} id="5">
               {ruutu5}
             </button>
-            <button onClick={(event) => ruutuaKlikattu(event)} id="6">
+            <button onClick={(event) => pelaajanVuoro(event)} id="6">
               {ruutu6}
             </button>
           </div>
           <div>
-            <button onClick={(event) => ruutuaKlikattu(event)} id="7">
+            <button onClick={(event) => pelaajanVuoro(event)} id="7">
               {ruutu7}
             </button>
-            <button onClick={(event) => ruutuaKlikattu(event)} id="8">
+            <button onClick={(event) => pelaajanVuoro(event)} id="8">
               {ruutu8}
             </button>
-            <button onClick={(event) => ruutuaKlikattu(event)} id="9">
+            <button onClick={(event) => pelaajanVuoro(event)} id="9">
               {ruutu9}
             </button>
           </div>
